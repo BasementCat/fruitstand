6e9654d78f6f90407d88a08cca0c7ea8158a316b
Alec Elton <alec.elton@gmail.com>
2014-05-03 19:50:29 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+def read(filen):
+    with open(os.path.join(os.path.dirname(__file__), filen), "r") as fp:
+        return fp.read()
+ 
+setup (
+    name = "fruitstand",
+    version = "0.1",
+    description = "Web-based digital signage (designed for the RPi + Raspbian)",
+    long_description = read("README.md"),
+    author = "Alec Elton",
+    author_email = "alec.elton@gmail.com",
+    url = "https://github.com/BasementCat/fruitstand",
+    packages = ["fruitstand", "tests"],
+    test_suite = "nose.collector",
+    install_requires = [],
+    tests_require = ["nose"]
+)
